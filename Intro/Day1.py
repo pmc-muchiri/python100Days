3cd1c0bdd658c6cf1bfb097eb572d4cc7101dc00
pmc muchiri <pmc.ac.ke@gmail.com>
2023-07-01 02:01:47 +0300

    added day folder for tracking assignment s during the day

--- Intro/Day1.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band name Generator.")
-city_name = input("What's the name of the city you grew up in?\n")
-pet_name = input("What's your pet's name?\n")
-
-band_name = city_name  + " " + pet_name
-
-print(f'Your Band name could be {band_name}')
