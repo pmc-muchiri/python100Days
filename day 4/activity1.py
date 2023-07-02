bd3f2227672594240db9b655fae3470fd9d81883
pmc muchiri <pmc.ac.ke@gmail.com>
2023-07-02 18:48:51 +0300

    Day four continues

---
+++ day 4/activity1.py
@@ -0,0 +1,11 @@
+# randomisation of number
+#randomInt = random.randint(1.10)
+#prints random integer
+#
+import random
+
+rand_side = random.randint(0, 1)
+if rand_side == 1:
+    print("Tails")
+else:
+    print("Heads")
---
+++ day 4/pythonlist.py
@@ -0,0 +1,12 @@
+# way of organising and store data in python
+# store data with same type/ categories in one variable.
+# want to have order in your data
+# list is a set of square brackets with many data inside []
+#fruits = ["mango", "oranges", "Pines"]
+#numbers = [1, 2, 3, 4, 5]
+#prime_numbers = [2, 3, 5, 7, 11, 13, 17]
+#animals = ["Dog", "Lion", "Kuku"]
+states_of_america = ["Califonia", "Penna", "NY", "Minnesota", "Arizona", "Hawaii", "New Mexico", "Uta", "Idaho"]
+print(states_of_america[6])
+
+states_of_america[1] = penni
